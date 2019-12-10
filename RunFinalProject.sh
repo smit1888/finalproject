8a1d0b2832144a1d6a3dd970f0debc9788d619cb
smit1888 <smit1888@algonquinlive.com>
2019-12-10 17:02:45 -0500

    finished step f

--- RunFinalProject.sh
+++ RunFinalProject.sh
@@ -12,3 +12,6 @@ git commit -m "step 5"
 
 curl -u "smit1888" https://api.github.com/user/repos -d '{"name":"FinalProjectRepository","private":"true"}'
 
+ git push --set-upstream https://github.com/smit1888/FinalProjectRepository.git master
+
+ 
